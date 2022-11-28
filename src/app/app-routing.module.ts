13dbe0230e9e912d14cbc9e2c49fb7d84038cba2
unknown <julifc2005@gmail.com>
2022-11-28 09:09:06 -0500

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AComponent } from 'src/components/a/a.component';
-import { BComponent } from 'src/components/b/b.component';
-import { DComponent } from 'src/components/d/d.component';
+import { AComponent } from 'src/app/components/a/a.component';
+import { BComponent } from 'src/app/components/b/b.component';
+import { DComponent } from 'src/app/components/d/d.component';
 
 
 const routes: Routes = [
